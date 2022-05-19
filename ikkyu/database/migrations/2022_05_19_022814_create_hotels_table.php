af0a19bcd4757bb6877597905e404564c3ddc896
seino-2022php <2022php42-student018@la-study.com>
2022-05-19 11:55:25 +0900

    hotelテーブルファイルの作成

--- ikkyu/database/migrations/2022_05_19_022814_create_hotels_table.php
+++ ikkyu/database/migrations/2022_05_19_022814_create_hotels_table.php
@@ -15,6 +15,12 @@ class CreateHotelsTable extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->string("name",50);
+            $table->int("hotel_type")->unsigned();
+            $table->string("adress",100);
+            $table->time("checkin_time");
+            $table->time("checkout_time");
+            $table->int("max_rooms");
             $table->timestamps();
         });
     }
