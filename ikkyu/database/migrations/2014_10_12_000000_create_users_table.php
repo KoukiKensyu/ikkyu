44b1f83ce68eb21378261dee47224eb148699c24
kouki <2022php42-student013@la-study.com>
2022-05-19 11:30:11 +0900

    データベースのマイグレーションファイルを作成

--- ikkyu/database/migrations/2014_10_12_000000_create_users_table.php
+++ ikkyu/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('tel', 20);
+            $table->date('birthday');
+            $table->string('password',50);
+            $table->string('address', 100);
             $table->rememberToken();
             $table->timestamps();
         });
