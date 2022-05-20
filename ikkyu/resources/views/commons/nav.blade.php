8acd6cd739a72454c8bb91589d776915d0de3aaa
kouki <2022php42-student013@la-study.com>
2022-05-20 13:45:29 +0900

    ユーザーログインを作成

--- ikkyu/resources/views/commons/nav.blade.php
+++ ikkyu/resources/views/commons/nav.blade.php
@@ -1,9 +1,9 @@
 <div class="d-flex flex-row-reverse bd-highlight">
     <div class="p-2 bd-highlight">
-        <a class="nav-link" href="#">ログアウト</a>
+        <a class="nav-link" href="{{route('logout')}}">ログアウト</a>
     </div>
     <div class="p-2 bd-highlight">
-        <a class="nav-link" href="/register_input">会員登録</a>
+        <a class="nav-link" href="{{ route('login') }}">会員登録</a>
     </div>
     <div class="p-2 bd-highlight">
         <a class="nav-link" href="/login_user">会員ログイン</a>
