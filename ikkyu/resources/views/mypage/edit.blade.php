2592e4174e49c4e1db928b5374d693e3ab239423
kouki <2022php42-student013@la-study.com>
2022-05-24 17:11:52 +0900

    入力画面の空のインプットをメッセージで知らせるように変更

--- ikkyu/resources/views/mypage/edit.blade.php
+++ ikkyu/resources/views/mypage/edit.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <button onclick="location.href='/mypage/index'">戻る</button>
-<form action="{{route('mypage.edit_confirmation', $user->id)}}" method="POST">
-@csrf
-<input type="hidden" name="id" value="{{$user->id}}">
-<table class="table">
-<tr><th>氏名：</th><td><input type="text" name="name" value="{{old('name',$user->name)}}"></td></tr>
-<tr><th>住所：</th><td><input type="text" name="address" value="{{old('address',$user->address)}}"></td></tr>
-<tr><th>電話番号：</th><td><input type="tel" name="tel" value="{{old('tel',$user->tel)}}"></td></tr>
-<tr><th>メールアドレス：</th><td><input type="text" name="email" value="{{old('email',$user->email)}}"></td></tr>
-<tr><th>生年月日：</th><td><input type="date" name="birthday" value="{{old('birthday',$user->birthday)}}"></td></tr>
-<!--
-<tr><th>変更前のパスワード：</th><td><input type="password" name="password" value=""></td></tr>
-<tr><th>変更後のパスワード：</th><td><input type="password" name="password_new" value=""></td></tr>
-<tr><th>確認用のパスワード：</th><td><input type="password" name="password_confirmation" value=""></td></tr>
--->
-<button type="submit">変更</button>
+<form class="needs-validation" action="{{route('mypage.edit_confirmation', $user->id)}}" method="POST">
+    @csrf
+    <input type="hidden" name="id" value="{{$user->id}}">
+    <table class="table">
+    <tr>
+        <th><label for="validationCustom01" class="form-label">氏名：</label></th>
+        <td><input class="form-control" id="validationCustom01" type="text" name="name" value="{{old('name',$user->name)}}" required></td>
+    </tr>
+    <tr>
+        <th><label for="validationCustom02" class="form-label">住所：</label></th>
+        <td><input class="form-control" id="validationCustom02" type="text" name="address" value="{{old('address',$user->address)}}" required></td>
+    </tr>
+    <tr>
+        <th><label for="validationCustom03" class="form-label">電話番号：</label></th>
+        <td><input class="form-control" id="validationCustom03" type="tel" name="tel" value="{{old('tel',$user->tel)}}" required></td>
+    </tr>
+    <tr>
+        <th><label for="validationCustom04" class="form-label">メールアドレス：</label></th>
+        <td><input class="form-control" id="validationCustom04" type="text" name="email" value="{{old('email',$user->email)}}" required></td>
+    </tr>
+    <tr>
+        <th><label for="validationCustom05" class="form-label">生年月日：</label></th>
+        <td><input class="form-control" id="validationCustom05" type="date" name="birthday" value="{{old('birthday',$user->birthday)}}" required></td>
+    </tr>
+    <!--
+    <tr><th>変更前のパスワード：</th><td><input type="password" name="password" value=""></td></tr>
+    <tr><th>変更後のパスワード：</th><td><input type="password" name="password_new" value=""></td></tr>
+    <tr><th>確認用のパスワード：</th><td><input type="password" name="password_confirmation" value=""></td></tr>
+    -->
+    <button class="btn" type="submit">変更</button>
 </form>
-<p></p>
 @endsection
--- ikkyu/resources/views/reserve/edit.blade.php
+++ ikkyu/resources/views/reserve/edit.blade.php
@@ -3,41 +3,43 @@
 @section('content')
 <h2>予約内容入力</h2>
 
-<form action="{{route('reserve.check')}}" method="post">
+<form class="needs-validation" action="{{route('reserve.check')}}" method="post">
     @csrf
     <table border='1'>
         <tbody>
             <tr>
-            <td>宿名</td>
-            <td>{{$hotel[0]->name}}</td>
+                <td><label for="validationCustom01" class="form-label">宿名</label></td>
+                <td>{{$hotel[0]->name}}</td>
             </tr>
             <tr>
-            <td>氏名</td>
-            <td><input type="text" name="name" value=""></td>
+                <td><label for="validationCustom02" class="form-label">氏名</label></td>
+                <td><input class="form-control" id="validationCustom02" type="text" name="name" value="" required></td>
             </tr> 
             <tr>
-            <td>予約部屋数</td>
-            <td><select name="rooms">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>部屋</td>
+                <td><label for="validationCustom03" class="form-label">予約部屋数</label></td>
+                <td><select class="form-select" id="validationCustom03" name="rooms" required>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>部屋</td>
             </tr>
             <tr>
-            <td>日程</td>
-            <td><input type="date" name="checkin_date">～<input type="date" name="checkout_date"></td>
+                <td><label for="validationCustom04" class="form-label">日程</label></td>
+                <td><input class="form-control" id="validationCustom04" type="date" name="checkin_date" required>
+                    ～
+                    <input class="form-control" id="validationCustom04" type="date" name="checkout_date" required></td>
             </tr> 
             <tr>
-            <td>合計金額</td>
-            <td>$$$$$円</td>
+                <td>合計金額</td>
+                <td>$$$$$円</td>
             </tr>
             
         </tbody>
     </table>
     <input type="hidden" name="hotel_id" value="{{$hotel[0]->id}}">
-    <button type="submit">確認画面へ</button>
+    <button class="btn" type="submit">確認画面へ</button>
 </form>
     <button onclick="location.href='/reserve/show/{{$hotel[0]->id}}'">戻る</button>
 @endsection
