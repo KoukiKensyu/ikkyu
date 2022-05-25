0b00696f6ecb0f21e1dea3a982aaf26cfaf622ae
seino-2022php <2022php42-student018@la-study.com>
2022-05-25 17:20:06 +0900

    退会確認画面で予約情報の表示

--- ikkyu/app/Http/Controllers/UserController.php
+++ ikkyu/app/Http/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Reservation;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
