df9d9d08e81d9516d47253ea3fec8ad0f5d13b46
kouki <2022php42-student013@la-study.com>
2022-05-30 17:03:21 +0900

    modified header

--- ikkyu/resources/views/user_home/index.blade.php
+++ ikkyu/resources/views/user_home/index.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('content')
-<!--<a href="../mypage/index">マイページ</a>-->
-<!-- Jumbotron 
-<div
+
+
+ <div
   class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white"
-  style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');"
+  style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/003.webp');"
 >
   <h1 class="mb-3 h2">会員ホーム</h1>
 
@@ -14,8 +14,6 @@
     老舗旅館からビジネスホテルまで簡単検索
   </p>
 </div>
- Jumbotron -->
-<h1>会員ホーム</h1>
 @include('commons/flash')
 <style>
     .modal-body{
@@ -48,9 +46,8 @@
                 <div class="col-5">
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#testModal">タイプ検索</button>
                 </div>
-                
             </div>
-        </div>
+    </div>
     <div class="modal fade" id="testModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -78,65 +75,64 @@
                 </div>
             </div>
         </div>
-            <button class=" bd-highlight btn btn-outline-primary btn-rounded active " type="submit">検索</button>
+            <button class=" bd-highlight btn btn-outline-primary btn-rounded active" type="submit">検索</button>
     </div>
     </form>
     </div>
     </div>
+
+
     <form action="">
-    <div class="table-responsive">
-    <table class="table table-hover align-middle">
-    <thead class="bg-light">
-    <tr><!--<th style="width: 10%;"></th>--><th style="width: 20%;"></th><th>宿名</th><th>宿タイプ</th><th>部屋数</th></tr>
-    </thead>
-    @foreach ($hotels as $key=>$hotel)
-    <?php  $remainRooms = $remaining_rooms[$key];?>    
-    <style>
-        a {display:block;}
-    </style>
-    @if($remainRooms >= 1)
-    <tbody class=" align-middle ">
-
-    <tr class="align-middle ">
-        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">
-        @if($hotel->id < 6 )
-        <img src="/images/{{$hotel->id}}.jpg" width="163" height="130" alt="">
-        @else<img src="/images/6.jpg" width="163" height="130" alt="">
-        @endif
-        </a></td>
-        <!--<td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}"><?php echo nl2br($hotel->comment); ?></a></td>-->
-        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}" class="text-primary">{{$hotel->name}}</a></td>
-        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">
-        @if($hotel->hotel_type == 0)
-            シティホテル
-        
-        @elseif($hotel->hotel_type == 1)
-            リゾートホテル
-    
-        @elseif($hotel->hotel_type == 2)
-            ビジネスホテル
-            
-        @elseif($hotel->hotel_type == 3)
-            旅館
-        
-        @elseif($hotel->hotel_type == 4)
-            民宿
-            
-        @elseif($hotel->hotel_type == 5)
-            ペンション
-            
-            @endif</a></td>
-
-
-    <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">{{$remainRooms .'部屋/'. $hotel->max_rooms .'部屋'}}</a></td></tr>
-    @endif
-    @endforeach
-    </tbody>
-    </table>
-    </div>
-    </div>
+        <div class="table-responsive">
+            <table class="table table-hover align-middle">
+                <thead class="bg-light">
+                    <tr><!--<th style="width: 10%;"></th>--><th style="width: 20%;"></th><th>宿名</th><th>宿タイプ</th><th>部屋数</th></tr>
+                </thead>
+                @foreach ($hotels as $key=>$hotel)
+                <?php  $remainRooms = $remaining_rooms[$key];?>    
+                <style>
+                    a {display:block;}
+                </style>
+                @if($remainRooms >= 1)
+                <tbody class=" align-middle ">
+                    <tr class="align-middle ">
+                        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">
+                        @if($hotel->id < 6 )
+                        <img src="/images/{{$hotel->id}}.jpg" width="163" height="130" alt="">
+                        @else<img src="/images/6.jpg" width="163" height="130" alt="">
+                        @endif
+                        </a></td>
+                        <!--<td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}"><?php echo nl2br($hotel->comment); ?></a></td>-->
+                        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}" class="text-primary">{{$hotel->name}}</a></td>
+                        <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">
+                        @if($hotel->hotel_type == 0)
+                            シティホテル
+                        
+                        @elseif($hotel->hotel_type == 1)
+                            リゾートホテル
+                    
+                        @elseif($hotel->hotel_type == 2)
+                            ビジネスホテル
+                            
+                        @elseif($hotel->hotel_type == 3)
+                            旅館
+                        
+                        @elseif($hotel->hotel_type == 4)
+                            民宿
+                            
+                        @elseif($hotel->hotel_type == 5)
+                            ペンション
+                            
+                            @endif</a></td>
 
 
+                    <td><a href="/reserve/show/{{$hotel->id}}.php?aaa={{$remainRooms}}&bbb={{request('checkin_date')}}&ccc={{request('checkout_date')}}">{{$remainRooms .'部屋/'. $hotel->max_rooms .'部屋'}}</a></td></tr>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        </div>
     </form>
     <p>{{ $hotels->appends(Request::all())->links() }}</p>
     </div>
